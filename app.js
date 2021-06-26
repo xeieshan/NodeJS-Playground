dc9b40addc81240f9503bf8aea9a60417ee1e962
Zeeshan Haider <xeieshan@gmail.com>
2021-06-27 02:29:19 +0500

    - Added all scripts for json.js ECMAScript2015.js arrow-challenge.js - utils.js utilities functions

---
+++ app.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const utils = require("./utils.js");
+const add = require("./utils.js");
+const sum = add(1, 4);
+fs.writeFileSync("notes.txt", "My name is Zeeshan.");
+fs.appendFileSync("notes.txt", "\nBro!!");
+fs.appendFileSync("notes.txt", "\nNah!!");
+console.log(sum);
+const validator = require('validator');
+const chalk = require("chalk");
+const utils = require("./utils.js");
+utils.log(chalk.green.bgWhite.bold.inverse(msg));
+
+utils.log(chalk.blue.inverse('isEmail: ' + validator.isEmail('ss@ss.com')));
+utils.log(chalk.red('isUrl: ' + validator.isURL('http:test.com')));
+
