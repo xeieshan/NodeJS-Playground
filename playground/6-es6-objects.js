77838d39143a42b246809788c0e08361d42d403a
Zeeshan Haider <xeieshan@gmail.com>
2021-07-29 03:05:20 +0500

    - Object destructuring

--- playground/6-es6-objects.js
+++ playground/6-es6-objects.js
@@ -1,5 +1,5 @@
 const utils = require('../utils.js')
-
+// Object property shorthand
 const name = 'Zeeshan'
 const age = 22
 
@@ -9,3 +9,24 @@ const user = {
     location: 'London'
 }
 utils.log(user);
+
+// Object destructuring
+
+const user1 = {
+    name: 'Zeeshan',
+    age: 11,
+    location: 'London, GB'
+}
+
+const {name: userName, age: userAge, location, rating = 2121} = user1;
+utils.log(userName);
+utils.log(userAge);
+utils.log(location);
+utils.log(rating);
+
+const transaction = (transactionType, {name, age}) => {
+    utils.log(transactionType);
+    utils.log(name);
+    utils.log(age);    
+}
+transaction('order',user1)
